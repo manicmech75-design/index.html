cfce54c763f8580a0b45b48866143d8345a90497
manicmech75-design <manicmech75@gmail.com>
2025-12-14 23:29:20 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,38 @@
+const CACHE_NAME = "cityflip-cache-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/sw.js",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
 
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k === CACHE_NAME ? null : caches.delete(k))))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).then((resp) => {
+        // cache new requests (safe for simple sites)
+        const copy = resp.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return resp;
+      }).catch(() => cached);
+    })
+  );
+});
